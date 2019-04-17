288d36f06c9c17e788a94a269ebdba0c6ddfbda7
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-04-17 21:24:44 +0700

    add

--- bai02/src/bai02/Bai02.java
+++ bai02/src/bai02/Bai02.java
@@ -21,18 +21,31 @@ public class Bai02 {
         Double moneyAmount = 1000000.0;
         Date date1 = new Date();
         long time1 = date1.getTime();               
+        
+        /*
         Integer i = 0;//i = iterator / index
-        while(i < 1000000) {
-            i = i + 1;
+        
+        while(i < Math.pow(10, 9)) {
+            i++;
             
-            Calculation.calculateMark(mark);//chay 
+            Calculation.calculateMark(mark);//chay 3925
 //            i++;
         }
+        */
+        Integer i = 0;
+        for(i = 0; i < Math.pow(10, 9); i++) {
+            Calculation.calculateMark(mark);//chay 4039ms
+        }
+        System.out.println("i = "+i);
         Date date2 = new Date();        
         long time2 = date2.getTime();
         System.out.println("time1 = "+time1);
         System.out.println("time2 = "+time2);
         System.out.println("Thoi gian tinh toan = "+(time2 - time1));
         //System.out.println("f(3) = "+Calculation.fX(100.0));
+        Calculation.sayHello("Alex");
+        Calculation.showAge(2);
+        Calculation.calculateDay(3);
+        Calculation.makeNumbers();
     }
 }
--- bai02/src/bai02/Calculation.java
+++ bai02/src/bai02/Calculation.java
@@ -5,6 +5,8 @@
  */
 package bai02;
 
+import java.util.ArrayList;
+
 public class Calculation {
     public static String calculateMark(Float mark) {
         String message = "";
@@ -26,5 +28,77 @@ public class Calculation {
         //Double result = 3*Math.pow(x, 10) - 2*x - 1;
         Double result = 3*Math.sin(Math.pow(x, 7)) - Math.sin(2*x) - 1;
         return result;
+    }
+    
+    //function without return value
+    public static void sayHello(String name) {
+        System.out.println("Chao mr "+name);
+        //return
+    }
+    public static void showAge(Integer age) {
+        /*
+        if(age < 18) {
+            age = 18;
+        }
+        */
+        age = (age < 18 || age == null) ? 18 : age;//ternary
+        System.out.println("Age = "+age);        
+    }
+    /*
+    
+    */
+    public static void calculateDay(Integer day) {
+        String result = "";
+        switch(day) {
+            case 1: {
+                result = "Sunday";
+                break;
+            }
+            case 2: {
+                result = "Monday";
+                break;
+            }
+            case 3: {
+                result = "Tuesday";
+                break;
+            }
+            case 4: {
+                result = "Wednesday";
+                break;
+            }
+            case 5: {
+                result = "Thursday";
+                break;
+            }
+            
+            case 6: {
+                result = "Friday";
+                break;
+            }
+            case 7: {
+                result = "Monday";
+                break;
+            }
+            default: {
+                result = "Unknown";
+                break;
+            }
+        }
+        System.out.println("Today is : "+result);
+    }
+    public static void makeNumbers() {
+        int numbers[] = {1, 4, 5,6, 7,9 };
+        ArrayList<Integer> odds = new ArrayList<Integer>();
+        ArrayList<Integer> evens = new ArrayList<Integer>();
+      
+        for(int i = 0; i < numbers.length; i ++) {
+            if(numbers[i] % 2 == 0) {
+                odds.add(numbers[i]);                
+            } else {
+                evens.add(numbers[i]);
+            }
+        }
+        System.out.println("So chan: "+odds);
+        System.out.println("So le: "+evens);
     }
 }
